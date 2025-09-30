0c08c6a4020718e585cac0559f095da2f08cc82b
Raimon <raimon.txollo@gmail.com>
2025-09-30 20:04:07 +0200

    Add Header component with its styles

---
+++ src/ui/components/Header/Header.tsx
@@ -0,0 +1,42 @@
+import "./Header.css";
+
+const Header: React.FC = () => {
+  const iconDesktop = "/images/icon-desktop.webp";
+  const iconMobile = "/images/icon-mobile.webp";
+
+  return (
+    <header className="header-container">
+      <img
+        className="icon"
+        alt="Simulant la bandera del Japó, perfil del cap i part del coll del Godzilla de la pel·lícula Shin Godzilla (2016) sobre rodona vermella"
+        srcSet={`${iconMobile} 86w, ${iconDesktop} 84w`}
+        sizes="(max-width: 599px) 86px, 84px"
+        src="/images/icon-mobile.webp"
+        height={86}
+        width={86}
+      />
+      <h1 aria-label="Godzilla Poblenou Fan Club">
+        <div className="title-container">
+          <div className="title-name">
+            <span>Godzilla</span>
+          </div>
+          <div className="title-surname">
+            <div>
+              <span>Poblenou</span>
+            </div>
+            <div className="title-surname--desktop">
+              <div>
+                <span>Fan</span>
+              </div>
+              <div>
+                <span>Club</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
